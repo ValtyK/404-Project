6d2bcf8e16222b1f6b1e7cfb9826bc0c3508f810
Matheo <math.cartalade@gmail.com>
2025-04-22 10:41:56 +0200

    On commence le code

--- syntaxe/analyse_syntaxique.c
+++ syntaxe/analyse_syntaxique.c
@@ -12,4 +12,95 @@
     void rec_suite_seqmelo();
 
 
+void rec_melo() {
+    rec_mea();
+}
+
+void rec_mea() {
+    if (lexeme_courant().nature != ACCO) {
+        printf("Erreur : parenthese ouvrante attendu (ligne %u, colonne %u)", lexeme_courant().ligne, lexeme_courant().colonne);
+        exit(1);
+    }
+    avancer();
+    rec_seqmelo();
+    if (lexeme_courant().nature != ACCF) {
+        printf("Erreur : parenthese fermante attendu (ligne %u, colonne %u)", lexeme_courant().ligne, lexeme_courant().colonne);
+        exit(1);
+    }
+}
+
+void rec_seqmelo() {  
+    rec_mesure();
+    if (lexeme_courant().nature != SEPMESURE) {
+        printf("Erreur : '|' attendu après une mesure (ligne %u, colonne %u)",lexeme_courant().ligne, lexme_courant().colonne);
+        exit(1);
+    }
+    avancer();
+    rec_suite_seqmelo();
+}
+
+void rec_mesure() {
+    if (lexeme_courant().nature != DUREE_RYTHMIQUE) {
+        printf("Erreur : DUREE_RYTHMIQUE attendu (ligne %u, colonne %u)",lexeme_courant().ligne, lexme_courant().colonne);
+        exit(1);
+    }
+    avancer();
+    if (lexeme_courant().nature != PARO) {
+        printf("Erreur : PARO attendu (ligne %u, colonne %u)",lexeme_courant().ligne, lexme_courant().colonne);
+        exit(1);
+    }
+    avancer();
+    rec_notes();
+    if (lexeme_courant().nature != PARF) {
+        printf("Erreur : PARF attendu (ligne %u, colonne %u)",lexeme_courant().ligne, lexme_courant().colonne);
+        exit(1);
+    }
+    avancer();
+    rec_suite_seqmelo();
+}
+
+void rec_notes() {
+    if (lexeme_courant().nature != NOTE) {
+        printf("Erreur : NOTE attendu (ligne %u, colonne %u)",lexeme_courant().ligne, lexme_courant().colonne);
+        exit(1);
+    }
+    avancer();
     
+    switch (lexeme_courant().nature) {
+        case ENTIER:
+            avancer();
+            switch (lexeme_courant().nature) {
+                case SEPNOTE:
+                case
+            }
+            break;
+        
+        case DIESE:
+            break;
+        
+        default:
+            printf("Erreur : ENTIER ou DIESE attendu (ligne %u, colonne %u)", lexeme_courant().linge, lexeme_courant().colonne);\
+            exit(1);
+    }
+}
+
+void rec_suite_seqmelo() {
+
+}
+
+
+
+// ------------------------------------------------------------------
+int analyer(char* nomFichier) {
+    demarrer(nomFichier);
+    rec_melo();
+
+    if (lexeme_courant().nature == FIN_SEQUENCE) {
+        printf("\nSyntaxe : OK");
+        printf("\n");
+        return 1;
+    } else {
+        printf("Syntaxe : Erreur..");
+        return 0;
+    }
+}
