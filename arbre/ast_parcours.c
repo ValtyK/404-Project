b281dafa2d25a8868b23610dd2bf3ad1767c934a
Matheo <math.cartalade@gmail.com>
2025-04-23 16:35:24 +0200

    make clean

--- arbre/ast_parcours.c
+++ arbre/ast_parcours.c
@@ -2,3 +2,27 @@
 #include <stdio.h>
 
 #include "../type_ast.h"
+
+void aff_operateur(TypeOp op){
+	switch (op) {
+		case N_AFF:
+			printf(" = ") ;
+			break;
+
+	} 
+}
+
+void afficherA(Ast expr) {
+	switch (expr->nature) {
+        case OPERATION:
+            printf("(");
+            afficherA(expr->gauche);
+            aff_operateur(expr->operateur) ;
+            afficherA(expr->droite);
+            printf(")");
+            break ;
+        case VALEUR:
+            printf("%d", expr->valeur);
+            break ;
+	}
+}
