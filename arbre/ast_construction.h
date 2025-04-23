6786847859d4826916fd31b87b702ceee8054f37
Matheo <math.cartalade@gmail.com>
2025-04-23 16:23:41 +0200

    ajout dans le .h

--- arbre/ast_construction.h
+++ arbre/ast_construction.h
@@ -3,3 +3,6 @@
 Ast creer_operation(TypeOp op, Ast oprd_gauche, Ast oprd_droit);
 Ast creer_id(char* nom_variable);
 Ast creer_mesure(Ast oprd_gauche, Ast oprd_droit);
+Ast creer_dr(Ast oprd_gauche, Ast oprd_droit, char* duree_rythmique);
+Ast creer_note(char* note, Ast oprd_gauche, Ast oprd_droit);
+Ast creer_entier(int valeur);
