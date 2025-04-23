78f58a6f21172f1beef63565eb8a7923a0ccf81f
Valentin S. <valentin.slotwinski@gmail.com>
2025-04-23 20:46:09 +0200

    gout caca

--- musique.h
+++ musique.h
@@ -48,7 +48,7 @@ void write_normalized_audio(FILE *file, int bits_per_sample);
 void init_audio_buffers(int sample_rate, int num_channels, double duration_sec);
 void free_audio_buffers(void);
 
-// Ecriture dans les buffers audio
+// Ecriture dans les buffers audio gout caca
 void generate_signal_perso(double t1, double t2, double freq, double amp, int sample_rate); // fonction en bordel qui m'a permi de comprendre des choses
 void generate_chord(double t1, double t2, const double *frequencies, int count, double amp, int sample_rate); // pour générer des accords
 void generate_envelope(double t1, double t2, double attack, double decay, double sustain, double release, int sample_rate); // generer de l'enveloppe
