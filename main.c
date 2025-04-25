2e7e157ce0783ef0b4e3e0463243d1dc995424e3
Valentin S. <valentin.slotwinski@gmail.com>
2025-04-25 03:09:45 +0200

    Essais temporaires dans main.c

--- main.c
+++ main.c
@@ -21,24 +21,33 @@ int main(int argc, char **argv) {
 
     // Exemple : un accord autour de LA3 (220 Hz), de 1s à 3s
     // ici je génère plusieurs accords en synthèse additive qui se superposent dans le .wav
-    generate_signal_perso(0.0, 3.0, 220.0, 3000.0, sample_rate);  // LA3
-    generate_signal_perso(1.0, 3.0, 220.0 * pow(2.0, 7.0 / 12.0), 3000.0, sample_rate);  // MI
-    generate_signal_perso(3.0, 5.0, 330.0 * pow(2.0, 7.0 / 12.0), 3000.0, sample_rate);  // MI
-    generate_signal_perso(5.0, 7.0, 110.0 * pow(2.0, 7.0 / 12.0), 3000.0, sample_rate);  // MI
+    // generate_signal_perso(0.0, 3.0, 220.0, 3000.0, sample_rate);  // LA3
+    // generate_signal_perso(1.0, 3.0, 220.0 * pow(2.0, 7.0 / 12.0), 3000.0, sample_rate);  // MI
+    // generate_signal_perso(3.0, 5.0, 330.0 * pow(2.0, 7.0 / 12.0), 3000.0, sample_rate);  // MI
+    // generate_signal_perso(5.0, 7.0, 110.0 * pow(2.0, 7.0 / 12.0), 3000.0, sample_rate);  // MI
     
     // Hop là faire des accords sympa
-    double freqs[] = {261.626, 329.628, 391.995}; // Do, Mi, Sol
-    generate_chord(7.0, 9.0, freqs, 3, 3000.0, sample_rate);
+    // double freqs[] = {261.626, 329.628, 391.995}; // Do, Mi, Sol
+    // generate_chord(7.0, 9.0, freqs, 3, 3000.0, sample_rate);
     
     // Utilisation de note_to_frequency
-    double note1 = note_to_frequency("C", 4);
-    double note2 = note_to_frequency("E", 4);
-    double note3 = note_to_frequency("G", 4);
-    printf("note1 = %.3f Hz\nnote2 = %.3f Hz\nnote3 = %.3f Hz\n", note1, note2, note3);
-    double freqs_2[] = {note1, note2, note3}; // Do, Mi, Sol
-    generate_chord(9.0, 11.0, freqs_2, 3, 3000.0, sample_rate);
+    // double note1 = note_to_frequency("C", 4);
+    // double note2 = note_to_frequency("E", 4);
+    // double note3 = note_to_frequency("G", 4);
+    // printf("note1 = %.3f Hz\nnote2 = %.3f Hz\nnote3 = %.3f Hz\n", note1, note2, note3);
+    // double freqs_2[] = {note1, note2, note3}; // Do, Mi, Sol
+    // generate_chord(9.0, 11.0, freqs_2, 3, 3000.0, sample_rate);
     
-    generate_envelope(0.0, 11.0, 30.0, 20.0, 80.0, 30.0, sample_rate);
+    // generate_envelope(0.0, 11.0, 30.0, 20.0, 80.0, 30.0, sample_rate);
+
+    double ma_freq = note_to_frequency("C", 4);
+    generate_signal(0, 2, ma_freq, 3000.0, sample_rate);
+    generate_signal(2, 4, ma_freq, 3000.0, sample_rate);
+    generate_signal(4, 11, ma_freq, 3000.0, sample_rate);
+    
+    generate_signal(6, 11, 440, 1000, sample_rate);
+    generate_signal(6, 11, 330, 1000, sample_rate);
+    generate_signal(6, 11, 220, 1000, sample_rate);
 
     write_normalized_audio(file, bits_per_sample);
 
