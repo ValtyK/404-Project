0c5894f65e4a885cb1556d2e49f2d9ed768970b8
Ohitop <davinroland37@gmail.com>
2025-04-25 04:02:21 +0200

    Modif pour mettre des Ast dans la TS mais ne marche pas

--- table_symboles/table_symboles.h
+++ table_symboles/table_symboles.h
@@ -3,6 +3,7 @@
 
 #define LIDF 50  // nombre maximum de caractéres pour un IDF 
 #define NBMAXSYMB 100  // nombre maximum d'IDF 
+#include "../arbre/type_ast.h"
 
 typedef enum { 
    entier,
@@ -12,7 +13,7 @@ typedef enum {
 
 typedef struct {
    char nom[LIDF] ; // nom d'un identificateur
-   char melo[LIDF]; // Soit c'est un string 
+   Ast melo; // Soit c'est un Ast
    int valeur;       // soit un entier
    double d_val;     // soit un double
    type_symbole type; // le type contenu dans la variable
@@ -25,20 +26,20 @@ void initTS();
 int estIntMelo(char *idf);
 // Prend un couple (idf, v) 
 //  renvoie 1 si type(v) = INT
-//  renvoie 0 si type(v) = MELO (=STRING) 
+//  renvoie 0 si type(v) = MELO
 // renvoie -1 si type(v) = DOULBE
 
 void afficheTS() ;
 // affiche le contenu complet de la TS 
 
-int estPresentTS(char *idf, char *u, int *v, double *w) ;
+int estPresentTS(char *idf, Ast *u, int *v, double *w) ;
 // si un couple (idf, v') est present dans la table alors 
 //      affecte v' à *u ou *v selon si c'est un INT ou un STRING
 //      estPresentTS renvoie vrai
 // sinon
 //      estPresentTS renvoie faux
 
-void insererMeloTS(char *idf, char *v); // version avec un string à insérer
+void insererMeloTS(char *idf, Ast *v); // version avec un string à insérer
 void insererIntTS(char *idf, int v); // version avec un int à insérer
 void insererDoubleTS(char *idf, double v); // version avec un double à insérer
 // si un couple (idf,v') est present dans la table alors 
